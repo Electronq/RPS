de003f66c6671f19b368ec2ec2d78b8da2ab450e
Electrones <electronescraft@gmail.com>
2022-02-28 15:32:05 +0100

    scoreboard added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function game(){
+    for(let gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++){
+        let playerHand = window.prompt("What is your hand?")
+        computerPlay = computerHand()
+        playRound(playerHand, computerPlay)
+        
+        
+    }
+    console.log(`Game over`)
+}
+
 
 
 
@@ -12,36 +23,58 @@ function computerHand(){
     }
 }
 
-
+let computerScore, playerScore, gamesPlayed
 
 function playRound(playerHand, computerPlay){
+    
+    
     console.log("Computer: " + computerPlay)
     console.log("You: " + playerHand)
 
+
     if(playerHand.toLowerCase() == "rock" && computerPlay == "rock" ){
         console.log(`Draw! Computer played ${computerPlay}`)
+        
     }else if (playerHand.toLowerCase() == "rock" && computerPlay == "paper"){
         console.log(`You LOSE! Computer played ${computerPlay}!  paper beats rock`)
+        ++computerScore
+
     }else if(playerHand.toLowerCase() == "rock" && computerPlay == "scissors"){
         console.log(`You WIN! Computer played ${computerPlay}, rock beats scissors`)
+        ++playerScore
+
     }else if(playerHand.toLowerCase() == "paper" && computerPlay == "rock"){
         console.log(`You WIN! Computer played ${computerPlay}, paper beats rock`)
+        ++playerScore
+
     }else if(playerHand.toLowerCase() == "paper" && computerPlay == "paper"){
         console.log(`Draw! Computer played ${computerPlay}`)
+        
     }else if(playerHand.toLowerCase() == "paper" && computerPlay == "scissors"){
         console.log(`You LOSE! Computer played ${computerPlay}`)
+        ++computerScore
+
     }else if(playerHand.toLowerCase() == "scissors" && computerPlay == "rock"){
         console.log(`You LOSE! Computer played ${computerPlay}`)
+        ++computerScore 
+
     }else if(playerHand.toLowerCase() == "scissors" && computerPlay == "paper"){
         console.log(`You WIN! Computer played ${computerPlay}`)
+        ++playerScore
+
     }else if(playerHand.toLowerCase() == "scissors" && computerPlay == "scissors"){
         console.log(`Draw! Computer played ${computerPlay}`)
+
+    }else{
+        console.log(`Do you know how to play?`)
     }
+
+    console.log(` `)
 }
     
 
-let computerPlay = computerHand()
-let playerHand = prompt("What is your hand?")
 
 
-console.log(playRound(playerHand, computerPlay))
+
+
+console.log(game())
